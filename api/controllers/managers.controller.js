cee781a915539da3228294b9adec4424a52525c1
ktr92 <kudashevtim@gmail.com>
2022-05-18 21:20:44 +0300

    TASK-2 [add] Index. AppList. Get Managers List

--- api/controllers/managers.controller.js
+++ api/controllers/managers.controller.js
@@ -1,9 +1,3 @@
 import * as axios from 'axios'
 
-module.exports.fetch = async (req, res) => {
-  await axios.get('https://api-broker.demo.ipotech.su/api/v1/bank/manager/list?page=1&limit=10').then(function (response) {
-    res.status(200).json(response)
-  }).catch(function (error) {
-    res.status(error.response.status).json(error.response.statusText)
-  })
-}
+module.exports.fetch = async (req, res) => {}
