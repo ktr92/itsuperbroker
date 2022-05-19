dc56fe88df53dc97812c535d5be1cd5d918f7639
ktr92 <kudashevtim@gmail.com>
2022-05-19 13:01:46 +0300

    TASK-2 [upd] Index. AppList. Refactoring

--- components/AppList.vue
+++ components/AppList.vue
@@ -1,39 +1,50 @@
 <template>
   <div class="container p-8">
-    <h1>Managers List</h1>
+    <h1 class="m-4">
+      Список кураторов
+    </h1>
     <table v-if="items" class="m-4">
       <tr
-        v-for="item in items.data.data"
+        v-for="item in items.data"
         :key="item.id"
         class="p-4"
       >
-        <td class="p-2 border-b-2"><b>ID</b>: {{ item.id }}</td>
-        <td class="p-2 border-b-2"><b>Имя</b>: {{ item.firstName }}</td>
-        <td class="p-2 border-b-2"><b>Банк</b>: {{ item.bank.name }}</td>
-        <td class="p-2 border-b-2"><b>Email</b>: {{ item.email }}</td>
+        <td class="p-2 border-b-2">
+          <b>ID</b>: {{ item.id }}
+        </td>
+        <td class="p-2 border-b-2">
+          <b>Имя</b>: {{ item.firstName }}
+        </td>
+        <td class="p-2 border-b-2">
+          <b>Банк</b>: {{ item.bank.name }}
+        </td>
+        <td class="p-2 border-b-2">
+          <b>Email</b>: {{ item.email }}
+        </td>
       </tr>
     </table>
     <button
-      @click="getManagers"
       class="bg-green-500 rounded text-white px-8 py-2"
-    >GET</button>
+      @click="getManagers"
+    >
+      Обновить список кураторов
+    </button>
   </div>
 </template>
 
 <script>
 export default {
-  data () {
-    return {
-      items: null
+  async fetch () {
+    await this.$store.dispatch('managers/fetch')
+  },
+  computed: {
+    items () {
+      return this.$store.getters['managers/managers']
     }
   },
-  async fetch ({ store }) {
-    this.items = await store.dispatch('managers/fetch')
-  },
   methods: {
-    async getManagers () {
-      this.items = await this.$store.dispatch('managers/fetch')
-      console.log(this.items)
+    getManagers () {
+      this.$fetch()
     }
   }
 }
--- store/managers.js
+++ store/managers.js
@@ -3,12 +3,16 @@ export const state = () => ({
 })
 
 export const mutations = {
+  setManagers (state, payload) {
+    state.managers = payload
+  }
 }
 
 export const actions = {
-  async fetch ({ dispatch }, id) {
+  async fetch ({ dispatch, commit }, id) {
     try {
-      return await this.$axios.get('https://api-broker.demo.ipotech.su/api/v1/bank/manager/list?page=1&limit=10')
+      const data = await this.$axios.get('https://api-broker.demo.ipotech.su/api/v1/bank/manager/list?page=1&limit=10')
+      commit('setManagers', data.data)
     } catch (e) {
       dispatch('setMessage', { value: 'Не удалось получить список менеджеров', type: 'error' }, { root: true })
     }
