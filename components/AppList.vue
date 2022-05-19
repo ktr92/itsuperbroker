59b8ee288f62ea6b1dea5e720e0f6d9bf1aafa8d
ktr92 <kudashevtim@gmail.com>
2022-05-19 14:19:27 +0300

    TASK-2 [upd] Index. AppList. Refactoring

--- components/AppList.vue
+++ components/AppList.vue
@@ -1,34 +1,39 @@
 <template>
   <div class="container p-8">
-    <h1 class="m-4">
+    <h1 class="my-4">
       Список кураторов
     </h1>
-    <table v-if="items" class="m-4">
-      <tr
-        v-for="item in items.data"
-        :key="item.id"
-        class="p-4"
+    <p v-if="$fetchState.pending">
+      Загрузка данных...
+    </p>
+    <div v-else>
+      <table v-if="items" class="my-4">
+        <tr
+          v-for="item in items.data"
+          :key="item.id"
+          class="p-4"
+        >
+          <td class="p-2 border-b-2">
+            <b>ID</b>: {{ item.id }}
+          </td>
+          <td class="p-2 border-b-2">
+            <b>Имя</b>: {{ item.firstName }}
+          </td>
+          <td class="p-2 border-b-2">
+            <b>Банк</b>: {{ item.bank.name }}
+          </td>
+          <td class="p-2 border-b-2">
+            <b>Email</b>: {{ item.email }}
+          </td>
+        </tr>
+      </table>
+      <button
+        class="bg-green-500 rounded text-white px-8 py-2"
+        @click="getManagers"
       >
-        <td class="p-2 border-b-2">
-          <b>ID</b>: {{ item.id }}
-        </td>
-        <td class="p-2 border-b-2">
-          <b>Имя</b>: {{ item.firstName }}
-        </td>
-        <td class="p-2 border-b-2">
-          <b>Банк</b>: {{ item.bank.name }}
-        </td>
-        <td class="p-2 border-b-2">
-          <b>Email</b>: {{ item.email }}
-        </td>
-      </tr>
-    </table>
-    <button
-      class="bg-green-500 rounded text-white px-8 py-2"
-      @click="getManagers"
-    >
-      Обновить список кураторов
-    </button>
+        Обновить список кураторов
+      </button>
+    </div>
   </div>
 </template>
 
